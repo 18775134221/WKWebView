7e88fcb90ceb173dd164636cf5a3051019f0899a
18775134221 <18775134221@163.com>
2016-12-19 08:26:00 +0800

    新增加载进度条

--- WKWebViewLearn/WKWebViewLearn/TestWkwebViewController.m
+++ WKWebViewLearn/WKWebViewLearn/TestWkwebViewController.m
@@ -16,6 +16,9 @@
     WKUserContentController *userContentController;
 }
 
+// 进度条
+@property (nonatomic, strong) UIProgressView *progressView;
+
 @end
 
 @implementation TestWkwebViewController
@@ -23,11 +26,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupUI];
     
+
+}
+
+- (void) setupUI {
+    [self setupWKWebView];
+    [self setupProgressView];
+}
+
+- (void) setupWKWebView {
     // 1.配置环境
     WKWebViewConfiguration * configuration = [[WKWebViewConfiguration alloc]init];
     userContentController =[[WKUserContentController alloc]init];
-//    configuration.userContentController = userContentController;
+    //    configuration.userContentController = userContentController;
     wkWebView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) configuration:configuration];
     wkWebView.allowsBackForwardNavigationGestures = YES;
     
@@ -53,17 +66,17 @@
     configuration.processPool = [[WKWebKitSupport sharedSupport] processPool];
     configuration.userContentController = userContentController;
 #warning 关于Cookie同步 WKWebView有自己的缓存机制,如果想同步session需要注意一下几个地方
-  
+    
     
 #warning 基本使用
     wkWebView.UIDelegate = self;
     wkWebView.navigationDelegate = self;
     
-//    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
-//    // 根据URL创建请求
-//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-//    [request setTimeoutInterval:15];
-//    [wkWebView loadRequest:request];
+    //    NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
+    //    // 根据URL创建请求
+    //    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    //    [request setTimeoutInterval:15];
+    //    [wkWebView loadRequest:request];
     
     
 #warning 在WKWebView加载请求的时候注入Cookie
@@ -88,12 +101,50 @@
     [requestObj setValue:cooki forHTTPHeaderField:@"Cookie"];
     [wkWebView loadRequest:requestObj];
 #warning   在WKWebView加载请求的时候注入Cookie
+    [wkWebView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progressView.progress = wkWebView.estimatedProgress;
+        if (self.progressView.progress == 1) {
+            /*
+             *添加一个简单的动画，将progressView的Height变为1.4倍，在开始加载网页的代理中会恢复为1.5倍
+             *动画时长0.25s，延时0.3s后开始动画
+             *动画结束后将progressView隐藏
+             */
+            __weak typeof (self)weakSelf = self;
+            [UIView animateWithDuration:0.25f delay:0.3f options:UIViewAnimationOptionCurveEaseOut animations:^{
+                weakSelf.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.4f);
+            } completion:^(BOOL finished) {
+                weakSelf.progressView.hidden = YES;
+                
+            }];
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+- (void) setupProgressView {
+    //进度条初始化
+    self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 64, [[UIScreen mainScreen] bounds].size.width, 2)];
+    self.progressView.backgroundColor = [UIColor blueColor];
+    //设置进度条的高度，下面这句代码表示进度条的宽度变为原来的1倍，高度变为原来的1.5倍.
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    [self.view addSubview:self.progressView];
 }
 
 #pragma mark - WKNavigationDelegate 方法较为常用
 // 页面开始加载时调用
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
-    
+    NSLog(@"开始加载网页");
+    //开始加载网页时展示出progressView
+    self.progressView.hidden = NO;
+    //开始加载网页的时候将progressView的Height恢复为1.5倍
+    self.progressView.transform = CGAffineTransformMakeScale(1.0f, 1.5f);
+    //防止progressView被网页挡住
+    [self.view bringSubviewToFront:self.progressView];
 }
 // 当内容开始返回时调用
 - (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
@@ -161,6 +212,7 @@
 - (void)dealloc{
     //这里需要注意，前面增加过的方法一定要remove掉。
     [userContentController removeScriptMessageHandlerForName:@"NativeMethod"];
+    [wkWebView removeObserver:self forKeyPath:@"estimatedProgress"];
     NSLog(@"释放");
 }
 #pragma mark - WKScriptMessageHandler
